d054a87007975ad84fa1bbece7a5d26004c2e10e
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-12-09 12:42:41 +0200

    HW is done. Added all functionality

--- contacts.js
+++ contacts.js
@@ -1,54 +1,65 @@
-const { doesNotMatch } = require('assert');
+const { v4: uuidv4 } = require('uuid');
 const fs = require('fs/promises');
 const path = require('path');
 
 const dbFileName = 'contacts.json';
-const dbFile = `db/${dbFileName}`;
-const contactsPath = path.join(__dirname, dbFileName);
+const contactsPath = path.join(__dirname, '/db', dbFileName);
 
-// (function listContacts() {
-//   fs.readFile(dbFile)
-//     .then(data => console.log(data.toString()))
-//     .catch(err => console.log(err.message));
-// })();
+function listContacts() {
+  fs.readFile(contactsPath)
+    .then(data => {
+      console.log(data.toString());
+      console.log('\x1b[32m Succcess');
+    })
+    .catch(err => console.log('\x1b[31m Error ', err.message));
+}
 
-// (function getContactById(contactId) {
-//   const [, , id] = process.argv;
-//   fs.readFile(dbFile).then(data => {
-//     const db = JSON.parse(data.toString());
-//     db.map(contact => {
-//       if (contact.id === id) {
-//         console.log(contact);
-//       }
-//     });
-//   });
-// })();
+function getContactById(contactId) {
+  fs.readFile(contactsPath)
+    .then(data => {
+      const db = JSON.parse(data.toString());
+      db.map(contact => {
+        if (contact.id === contactId.toString()) {
+          console.log(contact);
+          console.log('\n \x1b[32m Succcess');
+        }
+      });
+    })
+    .catch(err => console.log('\x1b[31m Error', err.message));
+}
 
-// function removeContact(contactId) {
-//   // ...твой код
-// }
+function removeContact(contactId) {
+  fs.readFile(contactsPath, 'utf8')
+    .then(data => {
+      const db = JSON.parse(data);
+      db.map(contact => {
+        if (contact.id === contactId.toString()) {
+          db.splice(db.indexOf(contact), 1);
+          fs.writeFile(contactsPath, JSON.stringify(db))
+            .then(() => console.log('\n \x1b[32m Removal successful'))
+            .catch(err => console.log('\x1b[31m Error', err.message));
+        }
+      });
+    })
+    .catch(err => console.log('\x1b[31m Error', err.message));
+}
 
-// (function removeContact(contactId) {
-//   const [, , id] = process.argv;
-//   fs.readFile(dbFile, 'utf8')
-//     .then(data => {
-//       const db = JSON.parse(data);
-//       db.map(contact => {
-//         if (contact.id === id) {
-//           db.splice(db.indexOf(contact), 1);
-//           fs.writeFile(dbFile, JSON.stringify(db));
-//         }
-//       });
-//     })
-//     .catch(err => console.log(err.message));
-// })();
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath, 'utf8')
+    .then(data => {
+      const db = JSON.parse(data);
+      const id = uuidv4();
+      db.push({ id, name, email, phone });
+      fs.writeFile(contactsPath, JSON.stringify(db));
+      console.log('Added contact: ');
+      getContactById(id);
+    })
+    .catch(err => console.log('\x1b[31m Error', err.message));
+}
 
-// (function addContact() {
-//   const [, , name, email, phone] = process.argv;
-//   fs.readFile(dbFile, 'utf8').then(data => {
-//     const db = JSON.parse(data);
-//     db.push({ name, email, phone });
-//     fs.writeFile(dbFile, JSON.stringify(db));
-//   });
-//     // Прикрутить уникальный айдишник из либы
-// })();
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
